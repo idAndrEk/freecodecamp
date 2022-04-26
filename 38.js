c21e36ac8bfd4d70f37c628d3147410a00d0b6fc
Andrey <AndrEkat96@yandex.ru>
2022-04-26 13:35:00 +0500

    tasks FCC

---
+++ 38.js
@@ -0,0 +1,10 @@
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+let wordBlanks = " Was " + myNoun + " very " + myAdjective + ". ";
+wordBlanks += "It " + myVerb + " " + myAdverb + " next to me."; // Change this line
+// Only change code above this line
+console.log(wordBlanks);
