7b310e69a8d14a279f9a3fcc8a3da386843edd5c
Andrey <AndrEkat96@yandex.ru>
2022-05-01 20:55:11 +0500

    tasks FCC

---
+++ 83.js
@@ -0,0 +1,8 @@
+const myDog = {
+    // Only change code below this line
+    "name": "DOG",
+    "legs": 3,
+    "tails": 1,
+    "friends": ["Water", "Dogs"]
+      // Only change code above this line
+  };
